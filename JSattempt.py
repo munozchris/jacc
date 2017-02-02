0cc892b73ca5042ff1d3a50b3f331398353c7d8a
munozchris <camunoz@uchicago.edu>
2017-02-01 23:54:32 -0600

    current webdriver attempt, selects 'American Culture'

---
+++ JSattempt.py
@@ -0,0 +1,18 @@
+from selenium import webdriver  
+from selenium.common.exceptions import NoSuchElementException  
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+
+import bs4  
+
+url = 'https://coursesearch.uchicago.edu/psc/prdguest/EMPLOYEE/HRMS/c/UC_STUDENT_RECORDS_FL.UC_CLASS_SEARCH_FL.GBL'
+browser = webdriver.Chrome()  
+browser.get(url)
+
+el = browser.find_element_by_id('win0divUC_CLSRCH_WRK2_SUBJECTctrl')
+
+for option in el.find_elements_by_tag_name('option'):
+    if option.text == 'American Culture':
+        option.click() # select() in earlier versions of webdriver
+        break

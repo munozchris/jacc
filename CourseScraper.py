2440dce80e67d29c44aca76d0d46f38bf75e6a52
munozchris <camunoz@uchicago.edu>
2017-02-06 00:48:02 -0600

    Click every course, and next 25 results

--- CourseScraper.py
+++ CourseScraper.py
@@ -17,24 +17,25 @@ submit = browser.find_element_by_id("UC_CLSRCH_WRK2_SEARCH_BTN") # find the subm
 
 for i in range(len(el.find_elements_by_tag_name('option'))): # iterate for the length of dropdown options
     el = browser.find_element_by_id('win0divUC_CLSRCH_WRK2_SUBJECTctrl') # avoid stale element exception
-    el.find_elements_by_tag_name('option')[i].click()  # click a dropdown menu item
+    el.find_elements_by_tag_name('option')[i+1].click()  # click a dropdown menu item
     submit = browser.find_element_by_id("UC_CLSRCH_WRK2_SEARCH_BTN") #avoid stale element exception
     submit.click() # submit department query
     sleep(15) # wait for page to load html
-    new_page = browser.page_source # get JavaScript-rendered HTML
+    #new_page = browser.page_source # get JavaScript-rendered HTML
     #soup = bs4.BeautifulSoup(new_page, "lxml")
     #course_info = soup.find_all(class_="ps_box-value") # scrape and print course info
     #for detail in course_info:
         #print(detail.text)
     try:
-        for k in range(10):
+        flag = True
+        while(flag):
             class_desc= browser.find_elements_by_css_selector("tr.ps_grid-row.psc_rowact")
-            for j in range(len(class_desc[25:26])):
+            for j in range(len(class_desc[1:-4])):
                 class_descs = browser.find_elements_by_css_selector("tr.ps_grid-row.psc_rowact")
                 class_descs[j].click()
                 sleep(5)
                 new_page_desc = browser.page_source
-                soup1 = bs4.BeautifulSoup(new_page, "lxml")
+                soup1 = bs4.BeautifulSoup(new_page_desc, "lxml")
                 course_info = soup1.find(id="UC_CLS_DTL_WRK_DESCRLONG$0")
                 print(course_info.text)
                 ret_btn = browser.find_element_by_id("UC_CLS_DTL_WRK_RETURN_PB$0")
@@ -44,15 +45,14 @@ for i in range(len(el.find_elements_by_tag_name('option'))): # iterate for the l
                 more_results = browser.find_element_by_id("UC_RSLT_NAV_WRK_SEARCH_CONDITION2$46$") # see if page has more than 25 results
                 more_results.click() # bring up next 25 results
                 sleep(5) # wait for page to load html
-                new_page = browser.page_source # get html from new page
-                soup = bs4.BeautifulSoup(new_page, "lxml")
-                course_info = soup.find_all(class_="ps_box-value")
-                for detail in course_info:
-                    print(detail.text)
-                more_results = browser.find_element_by_id("UC_RSLT_NAV_WRK_SEARCH_CONDITION2$46$")
+                #new_page = browser.page_source # get html from new page
+                #soup = bs4.BeautifulSoup(new_page, "lxml")
+                #course_info = soup.find_all(class_="ps_box-value")
+                #for detail in course_info:
+                    #print(detail.text)
             except NoSuchElementException: # continue if page has no more results to load
                 print('inner try')
-                continue
+                flag= False
     except NoSuchElementException: # continue if page has no more results to load
         print('outter try')
         continue

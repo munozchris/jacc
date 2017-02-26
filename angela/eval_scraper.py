c74a40c68576c0ebd70cf11349cd10e6d19a1055
Anne Rogers <amr@cs.uchicago.edu>
2017-02-26 17:49:00 -0600

    made some more progress on eval_scraping

--- angela/eval_scraper.py
+++ angela/eval_scraper.py
@@ -12,6 +12,10 @@ from util import *
 # the main scraper that calls this should store the list of evals possibly in a dict
 
 # this code starts based off the assumption that it has the soup object
+BIOS_eval = False
+Language_eval = False
+TA_eval = False
+Normal_eval = False
 
 # STAT 20000 eval sample
 url1 = 'https://evaluations.uchicago.edu/evaluation.php?id=53790'
@@ -28,8 +32,24 @@ course_name = re.findall(r'([A-Z\s0-9]*:)(.*)', course_full)[0][1][1:]
 course_section = soup.find(class_="section-title").text
 # splits multiple instructors
 instr_list = str(soup.findAll('strong', text='Instructor(s):')[0].nextSibling)[1:].split(';')
+formatted_instrs = []
+for full_name in instr_list:
+    name_parts = full_name.split(', ')
+    formatted_instrs.append(name_parts[1] + ' ' + name_parts[0])
 
-eval_dict['CourseId'], eval_dict['CourseName'] = course_code, course_name
+num_responses = int(str(soup.findAll('strong', text='Number of Responses:')[0].nextSibling).strip())
+
+eval_dict['CourseId'], eval_dict['CourseName'], eval_dict['Professors'] = course_code, course_name, formatted_instrs
+eval_dict['NumResponses'] = num_responses
+
+# Bio-type eval
+bio_time = soup.find('th', text='How many hours did you spend each week preparing for this class (including labs)? (0-9 + hours scale)').nextSibling.nextSibling:
+if bio_time is not None:
+    BIOS_eval = True
+    num_str = bio_time.contents[2]
+    num_list = re.findall(r'\d+\.\d*', num_str)
+    mean, stdv = int(num_list[0]), int(num_list[1])
+
 
 
 quit()

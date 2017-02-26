120db3b2d765d51276b09f6ff630a318cb97e81c
Anne Rogers <amr@cs.uchicago.edu>
2017-02-26 16:55:10 -0600

    eval_scraping until now

--- angela/eval_scraper.py
+++ angela/eval_scraper.py
@@ -2,14 +2,38 @@
 
 import bs4
 import re
+from util import *
 
 ### Usage: ###
 # Chris's code call's Jae's code to generate a list of urls for each eval corresponding to a class
 # Chris's then creates a soup object for each of the links in that list
 # then my code processes each soup object for the information and spits back a dictionary with the information
 # for that eval that can be processed into an sql database
+# the main scraper that calls this should store the list of evals possibly in a dict
 
 # this code starts based off the assumption that it has the soup object
 
-def get_eval_info
+# STAT 20000 eval sample
+url1 = 'https://evaluations.uchicago.edu/evaluation.php?id=53790'
+url2 = 'https://evaluations.uchicago.edu/evaluationLegacy.php?dept=BIOS&course=10451&section=01&quarter=SPG&year=2011'
+soup = get_soup(url1)
+
+# def get_eval_info(soup):
+eval_dict = {}
+keys = ['CourseId', 'CourseName', 'SectionId']
+
+course_full = soup.find(class_="eval-page-title").text
+course_code = re.findall(r'([A-Z\s0-9]*:)(.*)', course_full)[0][0][:-1]
+course_name = re.findall(r'([A-Z\s0-9]*:)(.*)', course_full)[0][1][1:]
+course_section = soup.find(class_="section-title").text
+# splits multiple instructors
+instr_list = str(soup.findAll('strong', text='Instructor(s):')[0].nextSibling)[1:].split(';')
+
+eval_dict['CourseId'], eval_dict['CourseName'] = course_code, course_name
+
+
+quit()
+
+
+
 
--- angela/util.py
+++ angela/util.py
@@ -1,4 +1,108 @@
 ## util function to filter out irrelevant evaluations by year 
+# tag finder code copied from Jae's code
 
 from selenium import webdriver  
 import bs4
+from login_save import get_auth_driver
+import urllib.parse
+import re
+    
+    
+threshold_year = 2011
+url2 = 'https://evaluations.uchicago.edu/index.php?EvalSearchType=option-number-search&Department=&CourseDepartment=AKKD&CourseNumber=10102&InstructorLastName=&advancedSearch=SEARCH'
+browser = get_auth_driver(url2)
+
+url3 = 'https://evaluations.uchicago.edu/index.php?EvalSearchType=option-number-search&Department=&CourseDepartment=CHEM&CourseNumber=26700&InstructorLastName=&advancedSearch=SEARCH'
+# browser.get(url3)
+url4 = 'https://evaluations.uchicago.edu/evaluation.php?id=54366'
+
+
+def get_driver():
+    return browser
+
+
+
+def get_soup(url):
+    browser.get(url)
+    html = browser.page_source
+    soup = bs4.BeautifulSoup(html, "lxml")
+    return soup
+
+
+
+def is_absolute_url(url):
+    '''
+    Is url an absolute URL?
+    '''
+    if url == "":
+        return False
+    return urllib.parse.urlparse(url).netloc != ""
+
+
+
+def convert_if_relative_url(current_url, new_url):
+    '''
+    Attempt to determine whether new_url is a relative URL and if so,
+    use current_url to determine the path and create a new absolute
+    URL.  Will add the protocol, if that is all that is missing.
+
+    Inputs:
+        current_url: absolute URL
+        new_url: 
+
+    Outputs:
+        new absolute URL or None, if cannot determine that
+        new_url is a relative URL.
+
+    Examples:
+        convert_if_relative_url("http://cs.uchicago.edu", "pa/pa1.html") yields 
+            'http://cs.uchicago.edu/pa/pa.html'
+
+        convert_if_relative_url("http://cs.uchicago.edu", "foo.edu/pa.html") yields
+            'http://foo.edu/pa.html'
+    '''
+    if new_url == "" or not is_absolute_url(current_url):
+        return None
+
+    if is_absolute_url(new_url):
+        return new_url
+
+    parsed_url = urllib.parse.urlparse(new_url)
+    path_parts = parsed_url.path.split("/")
+
+    if len(path_parts) == 0:
+        return None
+
+    ext = path_parts[0][-4:]
+    if ext in [".edu", ".org", ".com", ".net"]:
+        return "http://" + new_url
+    elif new_url[:3] == "www":
+        return "http://" + new_path
+    else:
+        return urllib.parse.urljoin(current_url, new_url)
+
+
+
+def get_eval_links(soup, threshold_year = 2011):
+    table = soup.find(lambda tag: tag.name=='table' and tag.has_attr('id') and tag['id']=="evalSearchResults")
+    rows = table.findAll(lambda tag: tag.name=='tr')
+
+    # note: it's find and findAll 
+    links = []
+    for row in rows[1:]:
+        quarter = row.contents[7].text
+        year = int(re.search(r'\d{4}', quarter).group())
+        year = int(re.search(r'\d{4}', quarter).group())
+        if year > threshold_year:
+            links.append(row.td.a['href'])
+
+
+    abs_urls = []
+    for link in links:
+        abs_link = convert_if_relative_url(url2, link)
+        abs_urls.append(abs_link)
+
+    return abs_urls
+
+def quit():
+    browser.quit()
